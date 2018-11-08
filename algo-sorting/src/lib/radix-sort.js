df70c23a43e5e5d26ce603a3b190f8d342b0c5f4
Daniel Frey <dann.frey@me.com>
2018-11-07 20:45:49 -0800

    fixed an error in radix

--- algo-sorting/src/lib/radix-sort.js
+++ algo-sorting/src/lib/radix-sort.js
@@ -4,18 +4,18 @@ module.exports = function radix(array) {
   const base = 10;
   let max = Number.NEGATIVE_INFINITY; // you know, because infinity isn't large enough
   let divider = 1;
-  let results = [];
+  let resultArray = [];
 
   while (divider === 1 || divider <= max) {
-    const bucket = [...Array(10)].map(() => []);
+    const digitBucket = [...Array(10)].map(() => []);
     for (let i = 0; i < array.length; i++) {
-      bucket[Math.floor((array[i] / divider) % base)].push(array[i]);
+      digitBucket[Math.floor((array[i] / divider) % base)].push(array[i]);
       if (array[i] > max) {
         max = array[i];
       }
     }
-    results = [].concat(...bucket);
+    resultArray = [].concat(...digitBucket);
     divider *= base;
   }
-  return results;
+  return resultArray;
 };
